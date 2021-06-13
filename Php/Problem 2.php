40755140d77d24711bb4a51b1830eb2fdbe6d509
Binty <tabassumnuzhat26@gmail.com>
2021-06-13 13:08:29 +0600

    Lab 3

---
+++ Php/Problem 2.php
@@ -0,0 +1,34 @@
+
+<html>
+<head>
+<title>Problem 2</title>
+</head>
+<body>
+
+<?php
+$marks = 90;
+if($marks >= 90)
+{
+echo "A+";
+}
+else if($marks >= 80)
+{
+echo "A";
+}
+else if($marks >= 70)
+{
+echo "B";
+}
+else if($marks >= 60)
+{
+echo "C";
+}
+else
+{
+echo "F";
+}
+
+?>
+
+</body>
+</html>
